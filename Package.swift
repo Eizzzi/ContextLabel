9d5d54ff11d06e4589ac6c82fa4a16ee3ab8ac99
Eizzzi <79688958+Eizzzi@users.noreply.github.com>
2024-03-25 21:44:13 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,32 +5,26 @@ let package = Package(    name: "EizzziContextLabel",
 
     // Which platforms and minimum deployment targets are supported
     // See: https://docs.swift.org/package-manager/PackageDescription/PackageDescription.html#supportedplatform
-    platforms: [
-        .iOS(.v14)
-    ],
+    platforms: [.iOS(.v10)],
 
     // The externaly visible build artifacts
     // See: https://docs.swift.org/package-manager/PackageDescription/PackageDescription.html#product
     products: [
         // The library that you can actually import        
         .library(
-            name: "EizzziContextLabel",
-            targets: [ "EizzziContextLabel" ]
+            name: "ContextLabel",
+            targets: [ "ContextLabel" ]
         )
     ],
+    dependencies: [],
     // The basic building blocks.
     // See: https://docs.swift.org/package-manager/PackageDescription/PackageDescription.html#target
     targets: [
         // Our package contains two targets, one for our library
         // code, and one for our tests:
         .target(
-            name: "EizzziContextLabel",
-            path: "src/Classes"
+            name: "ContextLabel",
+            path: "ContextLabel"
         ),
-
-        .testTarget(
-            name: "EizzziContextLabelTests",
-            dependencies: [ "EizzziContextLabel" ]
-        )
     ]
 )
